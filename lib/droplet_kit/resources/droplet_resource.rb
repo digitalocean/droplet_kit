59c2a01d995b4700b0f10c4dbe3e4c2c81b0fb81
Robert Ross <rross@digitalocean.com>
2014-08-01 17:54:28 -0700

    Add find and create methods to droplet resources.

--- lib/droplet_kit/resources/droplet_resource.rb
+++ lib/droplet_kit/resources/droplet_resource.rb
@@ -6,6 +6,19 @@ module DropletKit
         path '/v2/droplets'
         handler(200) { |response| DropletMapping.extract_collection(response.body, :read) }
       end
+
+      action :find do
+        verb :get
+        path '/v2/droplets/:id'
+        handler(200) { |response| DropletMapping.extract_single(response.body, :read) }
+      end
+
+      action :create do
+        verb :post
+        path '/v2/droplets'
+        body { |object| DropletMapping.representation_for(:create, object) }
+        handler(202) { |response| DropletMapping.extract_single(response.body, :read) }
+      end
     end
   end
 end
