0480e03c604595be7e78cf6b5057221c4af8a42b
Robert Ross <rross@digitalocean.com>
2014-08-03 20:29:31 -0700

    Add actions for droplets.

--- lib/droplet_kit/resources/droplet_action_resource.rb
+++ lib/droplet_kit/resources/droplet_action_resource.rb
@@ -12,6 +12,48 @@ module DropletKit
           handler(201, 200) { |response| ActionMapping.extract_single(response.body, :read) }
         end
       end
+
+      action :snapshot do
+        path '/v2/droplets/:droplet_id/actions'
+        verb :post
+        body { |hash| { type: 'snapshot', name: hash[:name] }.to_json }
+        handler(201, 200) { |response| ActionMapping.extract_single(response.body, :read) }
+      end
+
+      action :kernel do
+        path '/v2/droplets/:droplet_id/actions'
+        verb :post
+        body { |hash| { type: 'kernel', kernel: hash[:kernel] }.to_json }
+        handler(201, 200) { |response| ActionMapping.extract_single(response.body, :read) }
+      end
+
+      action :rename do
+        path '/v2/droplets/:droplet_id/actions'
+        verb :post
+        body { |hash| { type: 'rename', name: hash[:name] }.to_json }
+        handler(201, 200) { |response| ActionMapping.extract_single(response.body, :read) }
+      end
+
+      action :rebuild do
+        path '/v2/droplets/:droplet_id/actions'
+        verb :post
+        body { |hash| { type: 'rebuild', image: hash[:image] }.to_json }
+        handler(201, 200) { |response| ActionMapping.extract_single(response.body, :read) }
+      end
+
+      action :restore do
+        path '/v2/droplets/:droplet_id/actions'
+        verb :post
+        body { |hash| { type: 'restore', image: hash[:image] }.to_json }
+        handler(201, 200) { |response| ActionMapping.extract_single(response.body, :read) }
+      end
+
+      action :resize do
+        path '/v2/droplets/:droplet_id/actions'
+        verb :post
+        body { |hash| { type: 'resize', size: hash[:size] }.to_json }
+        handler(201, 200) { |response| ActionMapping.extract_single(response.body, :read) }
+      end
     end
   end
 end
--- spec/lib/droplet_kit/resources/droplet_action_resource_spec.rb
+++ spec/lib/droplet_kit/resources/droplet_action_resource_spec.rb
@@ -3,6 +3,20 @@ require 'spec_helper'
 RSpec.describe DropletKit::DropletActionResource do
   subject(:resource) { described_class.new(connection) }
   let(:droplet_id) { 1066 }
+  def json
+    {
+      "action" => {
+        "id" => 2,
+        "status" => "in-progress",
+        "type" => action,
+        "started_at" => "2014-07-29T14:35:27Z",
+        "completed_at" => nil,
+        "resource_id" => 12,
+        "resource_type" => "droplet",
+        "region" => "nyc1"
+      }
+    }.to_json
+  end
 
   include_context 'resources'
 
@@ -12,21 +26,6 @@ RSpec.describe DropletKit::DropletActionResource do
   ACTIONS_WITHOUT_INPUT.each do |action_name|
     describe "Action #{action_name}" do
       let(:action) { action_name }
-
-      def json
-        {
-          "action" => {
-            "id" => 2,
-            "status" => "in-progress",
-            "type" => action,
-            "started_at" => "2014-07-29T14:35:27Z",
-            "completed_at" => nil,
-            "resource_id" => 12,
-            "resource_type" => "droplet",
-            "region" => "nyc1"
-          }
-        }.to_json
-      end
 
       it 'performs the action' do
         request = stub_do_api("/v2/droplets/#{droplet_id}/actions", :post).with(
@@ -38,6 +37,96 @@ RSpec.describe DropletKit::DropletActionResource do
         expect(request).to have_been_made
         expect(returned_action.type).to eq(action_name)
       end
+    end
+  end
+
+  describe "Action snapshot" do
+    let(:action) { 'snapshot' }
+
+    it 'performs the action' do
+      request = stub_do_api("/v2/droplets/#{droplet_id}/actions", :post).with(
+        body: { type: action, name: 'Dwight' }.to_json
+      ).to_return(body: json, status: 201)
+
+      returned_action = resource.send(action, droplet_id: droplet_id, name: 'Dwight')
+
+      expect(request).to have_been_made
+      expect(returned_action.type).to eq(action)
+    end
+  end
+
+  describe "Action kernel" do
+    let(:action) { 'kernel' }
+
+    it 'performs the action' do
+      request = stub_do_api("/v2/droplets/#{droplet_id}/actions", :post).with(
+        body: { type: action, kernel: 1556 }.to_json
+      ).to_return(body: json, status: 201)
+
+      returned_action = resource.send(action, droplet_id: droplet_id, kernel: 1556)
+
+      expect(request).to have_been_made
+      expect(returned_action.type).to eq(action)
+    end
+  end
+
+  describe "Action rename" do
+    let(:action) { 'rename' }
+
+    it 'performs the action' do
+      request = stub_do_api("/v2/droplets/#{droplet_id}/actions", :post).with(
+        body: { type: action, name: 'newname.com' }.to_json
+      ).to_return(body: json, status: 201)
+
+      returned_action = resource.send(action, droplet_id: droplet_id, name: 'newname.com')
+
+      expect(request).to have_been_made
+      expect(returned_action.type).to eq(action)
+    end
+  end
+
+  describe "Action rebuild" do
+    let(:action) { 'rebuild' }
+
+    it 'performs the action' do
+      request = stub_do_api("/v2/droplets/#{droplet_id}/actions", :post).with(
+        body: { type: action, image: 'ubuntu-14-04-x86' }.to_json
+      ).to_return(body: json, status: 201)
+
+      returned_action = resource.send(action, droplet_id: droplet_id, image: 'ubuntu-14-04-x86')
+
+      expect(request).to have_been_made
+      expect(returned_action.type).to eq(action)
+    end
+  end
+
+  describe "Action restore" do
+    let(:action) { 'restore' }
+
+    it 'performs the action' do
+      request = stub_do_api("/v2/droplets/#{droplet_id}/actions", :post).with(
+        body: { type: action, image: 'ubuntu-14-04-x86' }.to_json
+      ).to_return(body: json, status: 201)
+
+      returned_action = resource.send(action, droplet_id: droplet_id, image: 'ubuntu-14-04-x86')
+
+      expect(request).to have_been_made
+      expect(returned_action.type).to eq(action)
+    end
+  end
+
+  describe "Action resize" do
+    let(:action) { 'resize' }
+
+    it 'performs the action' do
+      request = stub_do_api("/v2/droplets/#{droplet_id}/actions", :post).with(
+        body: { type: action, size: '1gb' }.to_json
+      ).to_return(body: json, status: 201)
+
+      returned_action = resource.send(action, droplet_id: droplet_id, size: '1gb')
+
+      expect(request).to have_been_made
+      expect(returned_action.type).to eq(action)
     end
   end
 end
