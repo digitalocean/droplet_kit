3f38f4453b6ccc70c57ad0712d025ae5b0f63cfb
Robert Ross <rross@digitalocean.com>
2014-08-06 17:03:22 -0700

    Use shorthand for action definitions.

--- lib/droplet_kit/resources/droplet_resource.rb
+++ lib/droplet_kit/resources/droplet_resource.rb
@@ -1,53 +1,38 @@
 module DropletKit
   class DropletResource < ResourceKit::Resource
     resources do
-      action :all do
-        verb :get
-        path '/v2/droplets'
+      action :all, 'GET /v2/droplets' do
+        query_keys :per_page, :page
         handler(200) { |response| DropletMapping.extract_collection(response.body, :read) }
       end
 
-      action :find do
-        verb :get
-        path '/v2/droplets/:id'
+      action :find, 'GET /v2/droplets/:id' do
         handler(200) { |response| DropletMapping.extract_single(response.body, :read) }
       end
 
-      action :create do
-        verb :post
-        path '/v2/droplets'
+      action :create, 'POST /v2/droplets' do
         body { |object| DropletMapping.representation_for(:create, object) }
         handler(202) { |response| DropletMapping.extract_single(response.body, :read) }
         handler(422) { |response| ErrorMapping.fail_with(FailedCreate, response.body) }
       end
 
-      action :delete do
-        verb :delete
-        path '/v2/droplets/:id'
+      action :delete, 'DELETE /v2/droplets/:id' do
         handler(204) { |response| true }
       end
 
-      action :kernels do
-        verb :get
-        path '/v2/droplets/:id/kernels'
+      action :kernels, 'GET /v2/droplets/:id/kernels' do
         handler(200) { |response| KernelMapping.extract_collection(response.body, :read) }
       end
 
-      action :snapshots do
-        verb :get
-        path '/v2/droplets/:id/snapshots'
+      action :snapshots, 'GET /v2/droplets/:id/snapshots' do
         handler(200) { |response| SnapshotMapping.extract_collection(response.body, :read) }
       end
 
-      action :backups do
-        verb :get
-        path '/v2/droplets/:id/backups'
+      action :backups, 'GET /v2/droplets/:id/backups' do
         handler(200) { |response| BackupMapping.extract_collection(response.body, :read) }
       end
 
-      action :actions do
-        verb :get
-        path '/v2/droplets/:id/actions'
+      action :actions, 'GET /v2/droplets/:id/actions' do
         handler(200) { |response| ActionMapping.extract_collection(response.body, :read) }
       end
     end
