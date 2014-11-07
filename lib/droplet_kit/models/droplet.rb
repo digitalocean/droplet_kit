1868df7d07c384ef52583f89ab15708e6b8582f0
Robert Ross <rross@digitalocean.com>
2014-11-06 22:53:19 -0500

    Refactor convenience methods to use a network_for method.

--- lib/droplet_kit/models/droplet.rb
+++ lib/droplet_kit/models/droplet.rb
@@ -15,19 +15,26 @@ module DropletKit
     attribute :private_networking
 
     def public_ip
-      network = networks.v4.find do |network|
-        network.type == 'public'
-      end
-
+      network = network_for(:v4, 'public')
       network && network.ip_address
     end
 
     def private_ip
-      network = networks.v4.find do |network|
-        network.type == 'private'
-      end
-
+      network = network_for(:v4, 'private')
       network && network.ip_address
+    end
+
+    private
+
+    def network_for(type, publicity)
+      networks = case type
+                 when :v4 then self.networks.v4
+                 when :v6 then self.networks.v6
+                 end
+
+      networks.find do |network|
+        network.type == publicity
+      end
     end
   end
 end
