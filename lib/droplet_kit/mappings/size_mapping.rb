c545cfc7c074819cbf5007f4fb53b3926b0abd2c
Janaka Wickramasinghe <janaka@ascensionit.com.au>
2015-06-12 10:37:06 +0530

    added memory,vcpus,disk,regions,available to sizes attributes

--- lib/droplet_kit/mappings/size_mapping.rb
+++ lib/droplet_kit/mappings/size_mapping.rb
@@ -7,9 +7,14 @@ module DropletKit
       root_key singular: 'size', plural: 'sizes', scopes: [:read]
 
       property :slug, scopes: [:read]
+      property :memory, scopes: [:read]
+      property :vcpus, scopes: [:read]
+      property :disk, scopes: [:read]
       property :transfer, scopes: [:read]
       property :price_monthly, scopes: [:read]
       property :price_hourly, scopes: [:read]
+      property :regions, scopes: [:read]
+      property :available, scopes: [:read]
     end
   end
 end
