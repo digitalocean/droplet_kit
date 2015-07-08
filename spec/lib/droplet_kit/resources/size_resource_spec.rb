24aa6147e91f114d7d81f420bb6ca8e0b337f961
J. Gomez <alambike@gmail.com>
2015-07-09 01:34:00 +0200

    More resource pagination specs
    to other resources that support pagination

--- spec/lib/droplet_kit/resources/size_resource_spec.rb
+++ spec/lib/droplet_kit/resources/size_resource_spec.rb
@@ -12,5 +12,10 @@ RSpec.describe DropletKit::SizeResource do
 
       expect(resource.all).to eq(expected)
     end
+
+    it_behaves_like 'a paginated index' do
+      let(:fixture_path) {'sizes/all'}
+      let(:api_path) {'/v2/sizes'}
+    end
   end
 end
