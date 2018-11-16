cdaab74ef09eb9ae70e4b617b87da4a731a914a2
Scott Crawford <scrawford@digitalocean.com>
2018-11-16 16:49:12 -0500

    add k8s routes to kubernetes_resources

--- lib/droplet_kit/resources/kubernetes_resource.rb
+++ lib/droplet_kit/resources/kubernetes_resource.rb
@@ -4,13 +4,46 @@ module DropletKit
 
     resources do
       action :all, 'GET /v2/kubernetes/clusters' do
-        handler(200) { |response| KubernetesMapping.extract_collection(response.body, :read) }
+      end
+
+      action :find, 'GET /v2/kubernetes/clusters/:cluster_id' do
+      end
+
+      action :create, 'POST /v2/kubernetes/clusters' do
+      end
+
+      action :config, 'GET /v2/kubernetes/clusters/:cluster_id/kubeconfig' do
+      end
+
+      action :update, 'PUT /v2/kubernetes/clusters/:cluster_id' do
+      end
+
+      action :upgrade, 'GET /v2/kubernetes/clusters/:cluster_id/upgrade' do
+      end
+
+      action :delete, 'DELETE /v2/kubernetes/clusters/:cluster_id' do
+      end
+
+      action :cluster_node_pools, 'GET /v2/kubernetes/clusters/:cluster_id/node_pools' do
+      end
+
+      action :cluster_find_node_pool, 'GET /v2/kubernetes/clusters/:cluster_id/node_pools/:pool_id' do
+      end
+
+      action :cluster_node_pool_create, 'POST /v2/kubernetes/clusters/:cluster_id/node_pools' do
+      end
+
+      action :cluster_node_pool_update, 'PUT /v2/kubernetes/clusters/:cluster_id/node_pools/:pool_id' do
+      end
+
+      action :cluster_node_pool_delete, 'DELETE /v2/kubernetes/clusters/:cluster_id/node_pools/:pool_id' do
+      end
+
+      action :cluster_node_pool_recycle, 'POST /v2/kubernetes/clusters/:cluster_id/node_pools/:pool_id/recycle' do
+      end
+
+      action :get_options, 'GET /v2/kubernetes/options' do
       end
     end
-
-    #
-    # def all(*args)
-    #   PaginatedResource.new(action(:all), self, *args)
-    # end
   end
 end
