e56659708613c9a88ef14530bbc55e48e29d0844
Scott Crawford <scrawford@digitalocean.com>
2018-11-16 16:46:36 -0500

    Add cluster all functionality
    * Adds the ability to get all kubernetes clusters running for a user.
    * Adds specs to test the clusters being returned.

--- lib/droplet_kit/resources/kubernetes_resource.rb
+++ lib/droplet_kit/resources/kubernetes_resource.rb
@@ -4,6 +4,7 @@ module DropletKit
 
     resources do
       action :all, 'GET /v2/kubernetes/clusters' do
+        handler(200) { |response| KubernetesMapping.extract_collection(response.body, :read) }
       end
 
       action :find, 'GET /v2/kubernetes/clusters/:id' do
@@ -48,6 +49,10 @@ module DropletKit
 
       action :get_options, 'GET /v2/kubernetes/options' do
       end
+    end
+
+    def all(*args)
+      PaginatedResource.new(action(:all), self, *args)
     end
   end
 end
