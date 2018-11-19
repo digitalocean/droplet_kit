ca54edadbcffc79efb7ffb4758f2b999c9471c93
Scott Crawford <scrawford@digitalocean.com>
2018-11-19 16:06:28 -0500

    add create method for clusters

--- lib/droplet_kit/mappings/kubernetes_mapping.rb
+++ lib/droplet_kit/mappings/kubernetes_mapping.rb
@@ -3,18 +3,18 @@ module DropletKit
     include Kartograph::DSL
     kartograph do
       mapping Kubernetes
-      root_key plural: 'kubernetes_clusters', scopes: [:read]
+      root_key plural: 'kubernetes_clusters', singular: 'kubernetes_cluster', scopes: [:read]
 
       property :id, scopes: [:read]
-      property :name, scopes: [:read]
-      property :region, scopes: [:read]
-      property :version, scopes: [:read]
+      property :name, scopes: [:read, :create]
+      property :region, scopes: [:read, :create]
+      property :version, scopes: [:read, :create]
       property :cluster_subnet, scopes: [:read]
       property :service_subnet, scopes: [:read]
       property :ipv4, scopes: [:read]
       property :endpoint, scopes: [:read]
-      property :tags, scopes: [:read]
-      property :node_pools, scopes: [:read]
+      property :tags, scopes: [:read, :create]
+      property :node_pools, scopes: [:read, :create]
     end
   end
 end
