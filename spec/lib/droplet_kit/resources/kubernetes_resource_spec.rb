a931e8dc07e94cd5f105a67dff305eafbf5f7655
Scott Crawford <scrawford@digitalocean.com>
2018-11-20 10:12:10 -0500

    add specs for the update request

---
+++ spec/lib/droplet_kit/resources/kubernetes_resource_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+RSpec.describe DropletKit::KubernetesResource do
+  subject(:resource) { described_class.new(connection: connection) }
+  include_context 'resources'
+
+  describe '#update' do
+    let(:path) { '/v2/kubernetes/clusters' }
+    let(:new_attrs) do
+      {
+        "name" => "new-test-name",
+        "tags" => ["new-test"]
+      }
+    end
+
+    context 'for a successful update' do
+      it 'returns the created cluster' do
+        cluster = DropletKit::Kubernetes.new(new_attrs)
+
+        as_hash = DropletKit::KubernetesMapping.hash_for(:update, cluster)
+        expect(as_hash['name']).to eq(cluster.name)
+        expect(as_hash['tags']).to eq(cluster.tags)
+
+
+        as_string = DropletKit::KubernetesMapping.representation_for(:update, cluster)
+        stub_do_api(path, :put).with(body: as_string).to_return(body: api_fixture('kubernetes/clusters/update'), status: 202)
+
+        updated_cluster = resource.update(cluster)
+        expect(updated_cluster.name).to eq("new-test-name")
+        expect(updated_cluster.tags).to match_array(["new-test"])
+      end
+    end
+  end
+end
