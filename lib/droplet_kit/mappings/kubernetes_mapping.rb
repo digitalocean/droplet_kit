58cc5eb8cdd01f53d3128685eeb52a43c50510a8
Scott Crawford <scrawford@digitalocean.com>
2018-11-20 10:01:19 -0500

    add update wrapper for clusters

--- lib/droplet_kit/mappings/kubernetes_mapping.rb
+++ lib/droplet_kit/mappings/kubernetes_mapping.rb
@@ -6,14 +6,14 @@ module DropletKit
       root_key plural: 'kubernetes_clusters', singular: 'kubernetes_cluster', scopes: [:read]
 
       property :id, scopes: [:read]
-      property :name, scopes: [:read]
+      property :name, scopes: [:read, :update]
       property :region, scopes: [:read]
       property :version, scopes: [:read]
       property :cluster_subnet, scopes: [:read]
       property :service_subnet, scopes: [:read]
       property :ipv4, scopes: [:read]
       property :endpoint, scopes: [:read]
-      property :tags, scopes: [:read]
+      property :tags, scopes: [:read, :update]
       property :node_pools, scopes: [:read]
     end
   end
