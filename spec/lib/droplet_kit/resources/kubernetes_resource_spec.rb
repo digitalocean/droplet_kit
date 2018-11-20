034d74420c4ea0db9abfa9e15bbaad3354da1711
Scott Crawford <scrawford@digitalocean.com>
2018-11-20 13:53:57 -0500

    add wrapper for kubeconfig

--- spec/lib/droplet_kit/resources/kubernetes_resource_spec.rb
+++ spec/lib/droplet_kit/resources/kubernetes_resource_spec.rb
@@ -160,4 +160,19 @@ RSpec.describe DropletKit::KubernetesResource do
       expect(response).to eq(true)
     end
   end
+
+  describe '#config' do
+    it 'returns a yaml string kubeconfig' do
+      response = Pathname.new('./spec/fixtures/kubernetes/clusters/kubeconfig.txt').read
+
+      stub_do_api('/v2/kubernetes/clusters/1/kubeconfig', :get).to_return(body: response)
+
+      config = resource.config(id: '1')
+
+      expect(config).to be_kind_of(String)
+
+      parsed_config = YAML.load(config)
+      expect(parsed_config.keys).to match_array(["apiVersion", "clusters", "contexts", "current-context", "kind", "preferences", "users"])
+    end
+  end
 end
