128240c2c9a6e62215e1581f0bc4ed3e208c50db
Scott Crawford <scrawford@digitalocean.com>
2018-12-03 09:24:19 -0500

    rename config method to kubeconfig

--- spec/lib/droplet_kit/resources/kubernetes_cluster_resource_spec.rb
+++ spec/lib/droplet_kit/resources/kubernetes_cluster_resource_spec.rb
@@ -161,17 +161,17 @@ RSpec.describe DropletKit::KubernetesClustersResource do
     end
   end
 
-  describe '#config' do
+  describe '#kubeconfig' do
     it 'returns a yaml string kubeconfig' do
       response = Pathname.new('./spec/fixtures/kubernetes/clusters/kubeconfig.txt').read
 
       stub_do_api('/v2/kubernetes/clusters/1/kubeconfig', :get).to_return(body: response)
 
-      config = resource.config(id: '1')
+      kubeconfig = resource.kubeconfig(id: '1')
 
-      expect(config).to be_kind_of(String)
+      expect(kubeconfig).to be_kind_of(String)
 
-      parsed_config = YAML.load(config)
+      parsed_config = YAML.load(kubeconfig)
       expect(parsed_config.keys).to match_array(["apiVersion", "clusters", "contexts", "current-context", "kind", "preferences", "users"])
     end
   end
