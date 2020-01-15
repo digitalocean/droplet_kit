a6cf33e0df869594ac11dd373b670150be01234a
Zach Gershman <zgershman@digitalocean.com>
2020-01-15 15:31:12 -0800

    make sure cookie_ttl_seconds always int

--- spec/lib/droplet_kit/models/loadbalancer_spec.rb
+++ spec/lib/droplet_kit/models/loadbalancer_spec.rb
@@ -1,27 +1,33 @@
 require 'spec_helper'
 
 RSpec.describe DropletKit::LoadBalancer do
-  let(:lb) { DropletKit::LoadBalancer.new }
+  let(:lb) { DropletKit::LoadBalancer.new(:sticky_sessions => {:cookie_ttl_seconds => "500"}) }
 
-  describe 'default' do
-    it 'sets redirect_http_to_https to false' do
+  describe 'redirect_http_to_https' do
+    it 'defaults to false' do
       expect(lb.redirect_http_to_https).to be false
     end
 
-    it 'sets enable_proxy_protocol to false' do
-      expect(lb.enable_proxy_protocol).to be false
-    end
-  end
-
-  describe 'setting' do
-    it 'sets redirect_http_to_https to false' do
+    it 'can be set to true' do
       lb.redirect_http_to_https = true
       expect(lb.redirect_http_to_https).to be true
     end
+  end
 
-    it 'sets enable_proxy_protocol to false' do
+  describe 'enable_proxy_protocol' do
+    it 'defaults to false' do
+      expect(lb.enable_proxy_protocol).to be false
+    end
+
+    it 'can be set to true' do
       lb.enable_proxy_protocol = true
       expect(lb.enable_proxy_protocol).to be true
+    end
+  end
+
+  describe 'sticky_sessions' do
+    it 'cookie_ttl_seconds is never a string' do
+      expect(lb.sticky_sessions.cookie_ttl_seconds).to eq 500
     end
   end
 end
