4f0f1e137a4256483315bd5da4d4fe96a633ad99
Viola Holownia <vholownia@digitalocean.com>
2020-03-31 18:49:45 -0400

    Add VPC v3 attributes

--- lib/droplet_kit/models/vpc.rb
+++ lib/droplet_kit/models/vpc.rb
@@ -1,7 +1,10 @@
 module DropletKit
   class VPC < BaseModel
     attribute :id
+    attribute :urn
     attribute :name
+    attribute :description
+    attribute :ip_range
     attribute :region
     attribute :created_at
     attribute :default
