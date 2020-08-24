339e26304f37e9c7a576aa1ff9c182eac93f173a
Mansur Berbero <mansurberbero@gmail.com>
2020-08-25 00:13:30 +0300

    Balance EP support added

---
+++ lib/droplet_kit/mappings/balance_mapping.rb
@@ -0,0 +1,14 @@
+module DropletKit
+  class BalanceMapping
+    include Kartograph::DSL
+
+    kartograph do
+      mapping Balance
+      property :month_to_date_balance, scopes: [:read]
+      property :account_balance, scopes: [:read]
+      property :month_to_date_usage, scopes: [:read]
+      property :generated_at, scopes: [:read]
+      
+    end
+  end
+end
