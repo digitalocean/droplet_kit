e99a32bb4440018d25c9d95c848c8804b9cf3b7e
Mansur Berbero <mansurberbero@gmail.com>
2020-08-31 18:40:30 +0300

    Invoice Ep added

---
+++ lib/droplet_kit/resources/invoice_resource.rb
@@ -0,0 +1,11 @@
+module DropletKit
+    class InvoiceResource < ResourceKit::Resource
+      include ErrorHandlingResourcable
+  
+      resources do
+        action :list, 'GET /v2/customers/my/invoices' do
+          handler(200) { |response| InvoiceMapping.extract_collection(response.body, :read) }
+        end
+      end
+    end
+  end
