01032e23f942548206f75e53d5a530634fd585f5
Ben Tranter <ben@bentranter.io>
2023-08-16 13:01:15 -0400

    restore rate limit err when retry-after missing

--- lib/droplet_kit/error_handling_resourcable.rb
+++ lib/droplet_kit/error_handling_resourcable.rb
@@ -7,6 +7,14 @@ module ErrorHandlingResourcable
         case response.status
         when 200...299
           next
+        when 429
+          unless response.headers.key?('Retry-After')
+            error = DropletKit::RateLimitReached.new("#{response.status}: #{response.body}")
+            error.limit = response.headers['RateLimit-Limit']
+            error.remaining = response.headers['RateLimit-Remaining']
+            error.reset_at = response.headers['RateLimit-Reset']
+            raise error
+          end
         else
           raise DropletKit::Error, "#{response.status}: #{response.body}"
         end
